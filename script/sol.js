b0abad0239f46b24470075636b750cea2aaeab59
dextermichaelh <dextermichael.h@gmail.com>
2018-10-14 16:49:04 -0400

    created player and enemy objects

--- script/sol.js
+++ script/sol.js
@@ -3,62 +3,126 @@ var canvas = document.querySelector('#canvas');
 var c = canvas.getContext("2d");
 var contoller; // controller
 var FPS;// loop
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-
- 
-
- 
-// c.fillRect(100, 100, 100, 100);
-
-
-//contoller
-// controller = {
-  
-//     left:false,
-//     right:false,
-//     up:false,
-//     keyListener:function(event) {
-  
-//       var key_state = (event.type == "keydown")?true:false;
-  
-//       switch(event.keyCode) {
-  
-//         case 37:// left key
-//           controller.left = key_state;
-//         break;
-//         case 38:// up key
-//           controller.up = key_state;
-//         break;
-//         case 39:// right key
-//           controller.right = key_state;
-//         break;
-  
-//       }
-  
-//     }
-  
-//   };
-
 var circlex = 200;  
 var circley = 00;
 var velocityx = 0;
 var velocityy = 2;
 var radius = 30;
 var friction = .9;
- var gravity = 1;
-
-  
+var gravity = 1;
 
 
-  
+
+c.font = '30px Arial';
+
+
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+
+
+
+
+
+
+
+
+
+
 
 //    Objects in the game
 
 
-function Shape(circlex, circley, velocityx, velocityy){
+//player
+
+var playerOne = {
+
+  x: 150,
+  y: 140,
+  dx: 1,
+  dy:1,
+  name:"Player"
+}
+
+
+
+function updatePlayer()
+{
+
+  playerOne.x += playerOne.dx;
+  playerOne.y += playerOne.dy;
+c.fillText(playerOne.name ,playerOne.x, playerOne.y);
+
+if(playerOne.x < 0 || playerOne.x > innerWidth){
+
+  playerOne.dx = -playerOne.dx;
+}
+
+if(playerOne.y < 0 || playerOne.y > innerHeight){
+
+  playerOne.dy = -playerOne.dy;
+  }
+
+
+
+}
+
+
+//enemy
+var enemy = {
+
+  x: 350,
+  y: 440,
+  dx: 1,
+  dy:1,
+  name:"enemy"
+
+}
+
+
+
+
+function updateEnemy()
+{
+
+  enemy.x += enemy.dx;
+  enemy.y += enemy.dy;
+c.fillText(enemy.name ,enemy.x, enemy.y);
+
+if(enemy.x < 0 || enemy.x > innerWidth){
+
+  enemy.dx = -enemy.dx;
+}
+
+if(enemy.y < 0 || enemy.y > innerHeight){
+
+  enemy.dy = -enemy.dy;
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function User(circlex, circley, velocityx, velocityy)
+
+{
+
+
+  
+
+
 
 this.circlex = circlex;
 this.circley = circley;
@@ -71,7 +135,7 @@ this.draw = function(){
 
   c.beginPath()
   c.arc(this.circlex,this.circley, this.radius, 30,0,Math.PI * 2, false);
-  c.strokeStyle = 'gold';
+  c.strokeStyle = 'black';
   c.fillStyle = 'blue';
   c.fill();
   c.stroke();
@@ -88,75 +152,59 @@ else {
 
 
 }
-
-
-
-
-  this.circlex += this.velocityx;
+   this.circlex += this.velocityx;
   this.circley += this.velocityy;
   this.draw()
 
      }
 
+
+
+     
+
      
 }
 
- var circle = new Shape(300,200, 0, 0, 0);
+ var player = new User(700,100, 0, 1,);
+
  
 
 
 
-
-// function drawCircle() {
-//   c.beginPath()
-//   c.arc(circlex,circley, radius, 30,0,Math.PI * 2, false);
-//   c.strokeStyle = 'red';
-//   c.fillStyle = 'red';
-//   c.fill();
-//   c.stroke();
-
-  
-//  }
-
-
-
-
-
-
-
-
+// Animation
 
 FPS = function() {
 
 
 c.clearRect(0, 0, innerWidth,innerHeight)
  requestAnimationFrame(FPS)
- 
-
- circle.update()
-//  drawCircle();
-  // creating collison
-//  if ( circlex + radius > innerWidth || circlex - radius < 0 )  {
-//     velocityx = -velocityx
-// }
- 
-// if  ( circley + radius > innerHeight)
-// {
-//   velocityy = -velocityy * friction;
-// } 
-// else {
-//   velocityy += gravity;
-// }
-
-
-
-//   circlex += velocityx;
-//   circley += velocityy;
- 
- 
+ updateEnemy()
+ updatePlayer()
+ player.update()
  
 
 }
+
+//  if (controller.up && )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
 
 FPS();
 
@@ -172,3 +220,11 @@ FPS();
   // window.addEventListener("keydown", controller.keyListener)
   // window.addEventListener("keyup", controller.keyListener);
   // window.requestAnimationFrame(FPS);
+
+
+
+
+
+
+
+ 
